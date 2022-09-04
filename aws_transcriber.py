640e2a48ebb04ea1a7567b682e46bddcb98ee398
Noah Gift <noah.gift@gmail.com>
2022-09-04 20:42:52 +0000

    download transcriber

--- aws_transcriber.py
+++ aws_transcriber.py
@@ -3,7 +3,8 @@
 
 import boto3
 import click
-
+import requests
+import json
 
 def transcribe_all_files(bucket_name, pattern="mp4"):
     """Transcribe all files in a bucket"""
@@ -35,18 +36,50 @@ def list_transcription_jobs():
     response = transcribe.list_transcription_jobs()
     return response["TranscriptionJobSummaries"]
 
-#write a function that retrieves the transcription job results
-def get_transcription_results(job_name):
-    """Get the results of a transcription job"""
+
+# get the uri of the transcription job to download the transcription
+def get_transcription_uri(job_name):
+    """Get the uri of a transcription job"""
 
     transcribe = boto3.client("transcribe")
     response = transcribe.get_transcription_job(TranscriptionJobName=job_name)
-    return response["TranscriptionJob"]["Transcript"]["TranscriptFileUri"]
+    uri_json = response["TranscriptionJob"]["Transcript"]["TranscriptFileUri"]
+    return uri_json
+
+
+# download the transcription using requests
+def download_transcription(uri_json, filename):
+    """Download a transcription"""
+
+    response = requests.get(uri_json)
+    with open(filename, "w") as f:
+        f.write(response.text)
+    
+# read the transcription from from a file and return only the text
+def read_transcription(filename):
+    """Read a transcription"""
+
+    with open(filename, "r") as f:
+        data = json.load(f)
+    return data["results"]["transcripts"][0]["transcript"]
+
+
+# write a function that takes a transcription job name, downloads the transcription, and returns the text
+def get_transcription_text(job_name):
+    """Get the text of a transcription"""
+
+    uri_json = get_transcription_uri(job_name)
+    #print(f"Downloading transcription from {uri_json}")
+    filename = f"{job_name}.json"
+    print(f"Saving transcription to {filename}")
+    download_transcription(uri_json, filename)
+    print(f"Transcription downloaded to {filename}")
+    text = read_transcription(filename)
+    return text
 
 @click.group()
 def cli():
     pass
-
 
 @cli.command("transcribe")
 @click.argument("bucket_name")
@@ -71,6 +104,18 @@ def list_jobs():
     print(f"Found {len(result)} jobs")
     for job in result:
         print(job["TranscriptionJobName"])
+
+
+@cli.command("get-results")
+@click.argument("job_name")
+def get_results(job_name):
+    """Get the results of a transcription job
+
+    Example: python aws_transcriber.py get-results my-job
+    """
+
+    result = get_transcription_text(job_name)
+    print(result)
 
 
 if __name__ == "__main__":
--- lambda_manager.py
+++ lambda_manager.py
@@ -3,7 +3,7 @@ import boto3
 import click
 
 
-#write a function that list all aws lambda functions created before a certain date
+# write a function that list all aws lambda functions created before a certain date
 def list_old_lambda_functions(year=2022):
     functions = []
     lambda_client = boto3.client("lambda")
@@ -15,8 +15,9 @@ def list_old_lambda_functions(year=2022):
             print(f"Found {func_name} created in {lambda_year}")
             functions.append(func_name)
     return functions
-    
-#write a function that deletes a list of lambda functions
+
+
+# write a function that deletes a list of lambda functions
 def delete_lambda_functions(functions):
     lambda_client = boto3.client("lambda")
     print(f"Deleting {len(functions)} functions with the following names: {functions}")
@@ -26,13 +27,18 @@ def delete_lambda_functions(functions):
         print(f"Response: {response}")
     return response
 
+
 @click.command()
-#add option to list old lambda functions
-@click.option("--list-lambda", is_flag=True, help="List all lambda functions over 1 year old")
-#add an option to delete the old functions
+# add option to list old lambda functions
+@click.option(
+    "--list-lambda", is_flag=True, help="List all lambda functions over 1 year old"
+)
+# add an option to delete the old functions
 @click.option("--delete", is_flag=True, help="Delete old lambda functions")
-#add an option to delete the old functions earlier than year
-@click.option("--year", default=2020, help="Delete old lambda functions earlier than year")
+# add an option to delete the old functions earlier than year
+@click.option(
+    "--year", default=2020, help="Delete old lambda functions earlier than year"
+)
 def main(delete, list_lambda, year):
     if list_lambda:
         print(list_old_lambda_functions())
@@ -40,9 +46,7 @@ def main(delete, list_lambda, year):
         functions = list_old_lambda_functions(year)
         delete_lambda_functions(functions)
 
+
 if __name__ == "__main__":
     # pylint: disable=no-value-for-parameter
     main()
-
-
-
