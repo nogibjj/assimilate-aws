edf50f134bc34f11cc54afd039949fe1a9966d4c
Noah Gift <noah.gift@gmail.com>
2022-09-04 20:13:02 +0000

    adding transcriber

--- aws_transcriber.py
+++ aws_transcriber.py
@@ -1,12 +1,72 @@
-import boto3
+#!/usr/bin/env python
 
-# build function to upload mp4 to s3 and transcribe
-def transcribe_mp4(filename):
-    # create s3 client
+
+import boto3
+import click
+
+
+def transcribe_all_files(bucket_name, pattern="mp4"):
+    """Transcribe all files in a bucket"""
+
     s3 = boto3.client("s3")
-    # upload file to s3
-    s3.upload_file(filename, "my-bucket", filename)
-    # create transcribe client
-    # transcribe = boto3.client("transcribe")
-    # start transcription job
-    # transcribe.start_transcription_job
+    transcribe = boto3.client("transcribe")
+    response = s3.list_objects_v2(Bucket=bucket_name)
+    for obj in response["Contents"]:
+        if pattern in obj["Key"]:
+            job_uri = f"s3://{bucket_name}/{obj['Key']}"
+            print(f"Transcribing {job_uri}")
+            key = obj["Key"]
+            print(f"Transcribing {key}")
+            response = transcribe.start_transcription_job(
+                TranscriptionJobName=key,
+                Media={"MediaFileUri": f"s3://{bucket_name}/{key}"},
+                MediaFormat="mp4",
+                LanguageCode="en-US",
+            )
+        print(f"Response: {response}")
+    #return a list of transcription job names
+    return [obj["Key"] for obj in response["Contents"] if pattern in obj["Key"]]
+
+#write a function that lists all the transcription jobs
+def list_transcription_jobs():
+    transcribe = boto3.client("transcribe")
+    response = transcribe.list_transcription_jobs()
+    return response["TranscriptionJobSummaries"]
+
+@click.group()
+def cli():
+    pass
+
+@cli.command("transcribe")
+@click.argument("bucket_name")
+def transcribe(bucket_name):
+    """Transcribe all files in a bucket
+    
+    Example: python aws_transcriber.py transcribe my-bucket
+
+    """
+    result = transcribe_all_files(bucket_name)
+    print(f"Transcribed {len(result)} files")
+
+@cli.command("list")
+def list():
+    """List all transcription jobs
+    
+    Example: python aws_transcriber.py list
+    """
+
+    result = list_transcription_jobs()
+    print(f"Found {len(result)} jobs")
+    for job in result:
+        print(job["TranscriptionJobName"])
+
+    
+    
+
+
+if __name__ == "__main__":
+    # pylint: disable=no-value-for-parameter
+    cli()
+
+
+
