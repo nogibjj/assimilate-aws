bc6a3346a220d33722043dad8aa9e8db9d69ac34
Noah Gift <noah.gift@gmail.com>
2022-09-06 19:30:28 +0000

    adding the start of C# on codespace for aws

---
+++ dotnetCode/MyApp/Program.cs
@@ -0,0 +1,16 @@
+﻿// print the number of buckets in my s3 account on AWS
+using System;
+using Amazon.S3;
+using Amazon.S3.Model;
+namespace MyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new AmazonS3Client();
+            var response = client.ListBucketsAsync().Result;
+            Console.WriteLine("Number of buckets: {0}", response.Buckets.Count);
+        }
+    }
+}
---
+++ dotnetCode/README.md
@@ -0,0 +1,9 @@
+# codespaces notes .NET and AWS
+
+Notes:  https://docs.microsoft.com/en-us/dotnet/core/install/linux-scripted-manual#scripted-install
+
+```
+wget https://dot.net/v1/dotnet-install.sh
+chmod +x dotnet-install.sh
+./dotnet-install.sh -c Current
+```
