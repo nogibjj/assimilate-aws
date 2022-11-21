5daea1b84ab6eb0e8badfed2453a4d1716843c09
Noah Gift <noah.gift@gmail.com>
2022-11-21 20:22:13 +0000

    adding sync

--- sync.py
+++ sync.py
@@ -1,11 +1,35 @@
 #!/usr/bin/env python
-"""Build a simply s3 directory copy tool.
+"""Sync a directory to s3 using subprocess
 
 """
-import os
-import boto3
+import subprocess
 import click
 
+#def build a function to sync a directory to s3 using subprocess and aws sync
+def sync_to_s3(source, bucket):
+    """Sync a directory to s3 using subprocess
+
+    """
+    subprocess.run(["aws", "s3", "sync", source, bucket])
+
+@click.group(
+    help="Sync a directory to s3 using subprocess"
+)
+def cli():
+    """Sync a directory to s3 using subprocess
+
+    """
+
+@cli.command("sync")
+@click.argument("source", default="test-sync-dir")
+@click.argument("bucket", default="s3://programatic-sync-test-11-21")
+def sync(source, bucket):
+    """Sync a directory to s3 using subprocess
+
+    """
+    sync_to_s3(source, bucket)
 
 
+if __name__ == "__main__":
+    cli()
 
